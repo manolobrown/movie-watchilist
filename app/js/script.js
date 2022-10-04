2880491445c58946b69820d41b0b1c0b908b02cc
manolobrown <manny3279@gmail.com>
2022-10-04 18:12:21 -0400

    [UPDATE] Code Clean Up

--- app/js/script.js
+++ app/js/script.js
@@ -72,9 +72,7 @@ if (form) {
                 <div class="meta">
                   <div class="run-time">${data.Runtime}</div>
                   <div class="genre">${data.Genre}</div>
-                  <button data-imdbID="${
-                    data.imdbID
-                  }" onClick="saveToWatchList('${data.imdbID}')">
+                  <button data-imdbid="${data.imdbID}">
                     <div class="icon">
                       <svg
                         width="16"
@@ -120,20 +118,49 @@ function saveToWatchList(id) {
 }
 
 function removeFromWatchList(id) {
-  filmIDs.filter((val) => val != id);
-  localStorage.setItem("films", JSON.stringify(filmIDs));
-}
-
-function getSavedFilms() {
   if (localStorage.getItem("films") == null) {
     return;
   }
 
-  watchList.innerHTML = "";
-
   const savedFilms = localStorage.getItem("films");
 
   const films = JSON.parse(savedFilms);
+
+  const filmIDs = films.filter((val) => val != id);
+
+  localStorage.setItem("films", JSON.stringify(filmIDs));
+
+  getSavedFilms();
+}
+
+function getSavedFilms() {
+  const savedFilms = localStorage.getItem("films");
+
+  const films = savedFilms ? JSON.parse(savedFilms) : [];
+
+  if (!films.length)
+    return (watchList.innerHTML = `<h2 class="heading-start">
+            Your watchlist is lookinga little empty...
+            <a href="/"
+              ><span class="icon"
+                ><svg
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM9 5C9 4.44772 8.55228 4 8 4C7.44772 4 7 4.44772 7 5V7H5C4.44772 7 4 7.44771 4 8C4 8.55228 4.44772 9 5 9H7V11C7 11.5523 7.44772 12 8 12C8.55228 12 9 11.5523 9 11V9H11C11.5523 9 12 8.55228 12 8C12 7.44772 11.5523 7 11 7H9V5Z"
+                    fill="white"
+                  /></svg></span
+              >Let’s add some movies!</a
+            >
+          </h2>`);
+
+  watchList.innerHTML = "";
 
   films.map((film) => {
     getFilmData(film).then((data) => {
@@ -166,26 +193,13 @@ function getSavedFilms() {
                 <div class="meta">
                   <div class="run-time">${data.Runtime}</div>
                   <div class="genre">${data.Genre}</div>
-                  <button data-imdbID="${
-                    data.imdbID
-                  }" onClick="saveToWatchList('${data.imdbID}')">
+                  <button data-remove-imdbid="${data.imdbID}">
                     <div class="icon">
-                      <svg
-                        width="16"
-                        height="16"
-                        viewBox="0 0 16 16"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          clip-rule="evenodd"
-                          d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM9 5C9 4.44772 8.55228 4 8 4C7.44772 4 7 4.44772 7 5V7H5C4.44772 7 4 7.44771 4 8C4 8.55228 4.44772 9 5 9H7V11C7 11.5523 7.44772 12 8 12C8.55228 12 9 11.5523 9 11V9H11C11.5523 9 12 8.55228 12 8C12 7.44772 11.5523 7 11 7H9V5Z"
-                          fill="white"
-                        />
-                      </svg>
+                      <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM5 7C4.44772 7 4 7.44772 4 8C4 8.55228 4.44772 9 5 9H11C11.5523 9 12 8.55229 12 8C12 7.44772 11.5523 7 11 7H5Z" fill="white"/>
+</svg>
                     </div>
-                    Watchlist
+                    Remove
                   </button>
                 </div>
                 <div class="description">
@@ -202,3 +216,46 @@ function getSavedFilms() {
 if (watchList) {
   getSavedFilms();
 }
+
+document.addEventListener("click", (e) => {
+  let imdbID = e.target.dataset.imdbid;
+  let imdbButton = e.target;
+  if (!imdbID) return;
+  imdbButton.innerText = "Added to List!";
+  imdbButton.disabled = true;
+  saveToWatchList(imdbID);
+});
+
+document.addEventListener("click", (e) => {
+  let imdbID = e.target.dataset.removeImdbid;
+  if (!imdbID) return;
+  removeFromWatchList(imdbID);
+});
+
+// class Menu {
+//   constructor(elem) {
+//     this._elem = elem;
+//     elem.onclick = this.onClick.bind(this); // (*)
+//   }
+
+//   save() {
+//     alert('saving');
+//   }
+
+//   load() {
+//     alert('loading');
+//   }
+
+//   search() {
+//     alert('searching');
+//   }
+
+//   onClick(event) {
+//     let action = event.target.dataset.action;
+//     if (action) {
+//       this[action]();
+//     }
+//   };
+// }
+
+// new Menu(menu);
